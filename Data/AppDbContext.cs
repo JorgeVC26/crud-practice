b925ad91b0395d0c1642426a0b806376e2478969
Jorge Villalobos <jorvillacar26@gmail.com>
2026-03-17 15:38:11 -0600

    create db context

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CrudPractice.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace CrudPractice.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
